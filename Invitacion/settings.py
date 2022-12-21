5ccfb9666360ab131ffae0dca369be7e4a27e891
julioznava <junavarro.rojas@gmail.com>
2022-12-21 00:42:13 -0300

    modificacion forms

--- Invitacion/settings.py
+++ Invitacion/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invitacionAPP'
+    'invitacionAPP',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- invitacionAPP/urls.py
+++ invitacionAPP/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', ingresonombre, name='ingresonombre'),
     path('index/', index, name='index'),
+    path('confirmar/', confirmar, name='confirmar'),
 ]
